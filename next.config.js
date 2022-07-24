bbe7e9fb79537d750352b3cdef83eb0aff133dc3
hws522 <sraphic@kakao.com>
2022-07-24 21:33:23 +0900

    add movie list

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: '/old-blog/:path*',
+        destination: '/new-sexy-blog/:path*',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `http://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: '/api/movies/:id',
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
